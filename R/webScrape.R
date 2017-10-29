0247f15aa2b10d0d45863e1febf0cedb8eaba447
Jason Timm <jaetimm@gmail.com>
2017-10-29 12:13:32 -0600

    to is.na

--- R/webScrape.R
+++ R/webScrape.R
@@ -45,6 +45,6 @@ GetWebTexts <- function(y) {
     lapply(z, boilerpipeR::DefaultExtractor)%>%
     gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "",., perl=TRUE)%>%
     gsub("\\\n"," ",., perl=TRUE)})
-  Filter(length,output)
+  output[!sapply(output, is.null)])
 }
 
