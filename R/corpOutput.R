a6a6e5e6694252ecb6133f39b66348f3cdbf51cb
Jason Timm <jaetimm@gmail.com>
2017-11-20 16:13:21 -0700

    add PROPN

--- R/corpOutput.R
+++ R/corpOutput.R
@@ -52,7 +52,7 @@ GetBOW <- function (x,contentOnly) {
   if (contentOnly==TRUE) {
     lapply(1:length(output), function(z){
       output[[z]]%>%
-        filter(pos %in% c("ADJ","NOUN","VERB","ADV"),!lemma %in% corpusdatr::stops)})
+        filter(pos %in% c("ADJ","NOUN","VERB","ADV","PROPN"),!lemma %in% corpusdatr::stops)})
   } else {return(output)}
 }
 
