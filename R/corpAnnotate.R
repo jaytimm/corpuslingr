88d7f9cde1067c6981ed9c994c1a643b6229f04f
Jason Timm <jaetimm@gmail.com>
2017-12-11 11:36:42 -0700

    edit

--- R/corpAnnotate.R
+++ R/corpAnnotate.R
@@ -42,13 +42,13 @@ annotation <- lapply(x, function(y){
   out <- y %>%
     mutate(token=gsub("\\s*","",token),
            lemma=gsub("\\s*","",lemma))%>%
-    mutate(token=gsub("_"," ",token),
-           lemma=gsub("_"," ",lemma))%>%
     mutate(lemma=ifelse(pos=="PROPN"|pos=="ENTITY",token,lemma))%>%
     mutate(lemma=gsub("xxx","-",lemma),
            token=gsub("xxx","-",token))%>%
     mutate(tag = ifelse(tag=="ENTITY",paste("NE",substr(entity_type,1,2),sep=""),tag))%>%
-    buildTuple()
+    buildTuple() %>%
+    mutate(token=gsub("_"," ",token),
+           lemma=gsub("_"," ",lemma))
 
   class(out) <- c("spacyr_parsed", "data.frame")
   return(out)})
