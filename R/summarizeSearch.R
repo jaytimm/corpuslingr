e2a679de3d7a9b21ef07b9c6c4052b130131fa43
Jason Timm <jaetimm@gmail.com>
2018-01-02 10:34:32 -0700

    re

--- R/summarizeSearch.R
+++ R/summarizeSearch.R
@@ -23,8 +23,7 @@ FlattenContexts <- function(x) {
 #' @export
 #' @rdname summarizeSearch
 GetSearchFreqs <- function (x,aggBy=c('lemma','token')) {
-    x%>%
-    data.table()%>%
+    data.table(x)%>%
     .[, list(txtf=length(eg),docf=length(unique(doc_id))),by=aggBy]%>%
     setorderv(.,c('txtf',aggBy),c(-1,rep(1,length(aggBy))))
     }
