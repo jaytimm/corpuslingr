354007e6cc2a833160fec35792b77e2ede410037
Jason Timm <jaetimm@gmail.com>
2018-01-03 10:08:16 -0700

    re

--- R/summarizeSearch.R
+++ R/summarizeSearch.R
@@ -36,7 +36,7 @@ GetSearchFreqs <- function (x,aggBy=c('lemma','token')) {
 GetKWIC <- function (x) {
 
     data.table(x$contexts)%>%
-    .[, kwic = paste(aContext,"<mark>",token,"</mark>",zContext,collapse=" "), by=list(doc_id,eg)]%>%
+    .[, list(kwic = paste(aContext,"<mark>",token,"</mark>",zContext,collapse=" ")), by=list(doc_id,eg)]%>%
     select(doc_id,token,kwic)%>%
     DT::datatable(class = 'cell-border stripe', rownames = FALSE,width="100%", escape=FALSE)
   }
