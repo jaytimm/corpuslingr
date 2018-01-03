5880737663f2cb8702273cf875872977731db549
Jason Timm <jaetimm@gmail.com>
2018-01-03 11:48:59 -0700

    re

--- R/summarizeSearch.R
+++ R/summarizeSearch.R
@@ -46,11 +46,13 @@ GetKWIC <- function (x) {
 #' @rdname summarizeSearch
 GetBOW <- function (x,contentOnly=TRUE, aggBy=c('lemma','pos')) {
 
-  bow <- data.table(x$BOW)
   if (contentOnly==TRUE) {
-    bow <- filter(bow,pos %in% c("ADJ","NOUN","VERB","ADV","PROPN","ENTITY"),!lemma %in% corpusdatr::stops)}
+    bow <- filter(x$BOW,pos %in% c("ADJ","NOUN","VERB","ADV","PROPN","ENTITY"),!lemma %in% corpusdatr::stops)
+  } else
+      {bow <- x$BOW}
 
-  bow <- bow[place!='token', cofreq=length(eg), by=aggBy]%>%
+  bow <- data.table(bow)%>%
+    .[place!='token', list(cofreq=length(eg)), by=aggBy]%>%
     setorderv(.,c('cofreq',aggBy),c(-1,rep(1,length(aggBy))))
 
  return(bow)}
