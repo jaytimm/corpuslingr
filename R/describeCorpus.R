224fa8fed56eb88296de254cc1134af70dda47d6
Jason Timm <jaetimm@gmail.com>
2018-01-27 11:40:02 -0700

    re

--- R/describeCorpus.R
+++ R/describeCorpus.R
@@ -13,8 +13,12 @@ GetDocDesc <- function (x) {
   if (!is.data.frame(x)) {x <- rbindlist(x)}
   x <- as.data.table(x)
 
-  x[pos!="PUNCT", list(textLength=length(token),textType=length(unique(token)),textSent=length(unique(sentence_id))), by=list(doc_id)]
+  byText <- x[pos!="PUNCT", list(textLength=length(token),textType=length(unique(token)),textSent=length(unique(sentence_id))), by=list(doc_id)]
 
+  corpus <- x[pos!="PUNCT", list(n_docs=length(unique(doc_id)),textLength=length(token),textType=length(unique(token)),textSent=length(unique(paste(doc_id,sentence_id, sep=""))))]
+
+  out <- list("text" = byText, "corpus" = corpus)
+  return(out)
 }
 
 
