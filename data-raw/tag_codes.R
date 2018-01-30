80397b3b1d44e6de9316787308c04189f2c22088
Jason Timm <jaetimm@gmail.com>
2018-01-30 10:00:35 -0700

    re

--- data-raw/tag_codes.R
+++ data-raw/tag_codes.R
@@ -3,6 +3,8 @@
 #devtools::use_data_raw()
 
 clr_tag_codes <- read.csv("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\tag_codes.csv")
+clr_upos_codes <- read.csv("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\upos_codes.csv")
 
 #Output
 devtools::use_data(clr_tag_codes, overwrite=TRUE)
+devtools::use_data(clr_pos_codes, overwrite=TRUE)
