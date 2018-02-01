4499cd279fa72fe0fea0738f0ac6f079a2b69a6b
Jason Timm <jaetimm@gmail.com>
2018-02-01 12:05:29 -0700

    re

--- data-raw/tags_stops.R
+++ data-raw/tags_stops.R
@@ -5,6 +5,11 @@
 clr_tag_codes <- read.csv("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\tag_codes.csv")
 clr_upos_codes <- read.csv("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\upos_codes.csv")
 
+
+clr_stops <- scan("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\corenlp_stops.txt",what="char",sep="\n")
+
+
 #Output
 devtools::use_data(clr_tag_codes, overwrite=TRUE)
 devtools::use_data(clr_upos_codes, overwrite=TRUE)
+devtools::use_data(clr_stops, overwrite=TRUE)
