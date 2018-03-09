25d88a805f6f7987ca4a07b2bd32863b1597879d
Jason Timm <jaetimm@gmail.com>
2018-03-09 14:39:04 -0700

    re

--- R/scrapeWeb.R
+++ R/scrapeWeb.R
@@ -73,8 +73,8 @@ clr_web_scrape <- function(y,link_var='link') {
   cleaned <- lapply(raws, function(z) {
     x <- lapply(z, boilerpipeR::ArticleExtractor)
     x <- gsub("\\\n"," ",x, perl=TRUE) #Note. Kills text structure.
-    x <- gsub("\\\"","\"",x, perl=TRUE)
-    gsub('share menu.*$|sharemenu.*$','',x)
+    gsub("\\\"","\"",x, perl=TRUE)
+    #gsub('share menu.*$|sharemenu.*$','',x)
       })
 
   names(cleaned) <- y[[link_var]]
