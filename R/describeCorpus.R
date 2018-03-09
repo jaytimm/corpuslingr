8aad022ca1902b0c6a890adb32ccb00c367abf0d
Jason Timm <jaetimm@gmail.com>
2018-03-09 14:09:22 -0700

    re

--- R/describeCorpus.R
+++ R/describeCorpus.R
@@ -15,6 +15,8 @@ clr_desc_corpus <- function (x,doc ='id',sent='sid', tok='word',upos='upos') {
 
   byText <- x[upos!="PUNCT", list(textLength=.N,textType=length(unique(get(tok))),textSent=length(unique(get(sent)))), by=doc]
 
+  byText <- byText[order(as.numeric(gsub('\\D+','',get(doc))))]
+
   corpus <- x[upos!="PUNCT", list(n_docs=length(unique(get(doc))),textLength=.N,textType=length(unique(get(tok))),textSent=length(unique(paste(get(doc),get(sent), sep=""))))]
 
   out <- list("text" = byText, "corpus" = corpus)
