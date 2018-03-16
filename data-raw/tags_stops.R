cd7d8eb02d50c4d0dfcf482fd7d50e080681e237
Jason Timm <jaetimm@gmail.com>
2018-03-16 12:13:37 -0600

    re

--- data-raw/tags_stops.R
+++ data-raw/tags_stops.R
@@ -1,14 +1,18 @@
 
 
+library(tidyverse)
 #devtools::use_data_raw()
 
-clr_tag_codes <- read.csv("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\tag_codes.csv")
-clr_upos_codes <- read.csv("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\upos_codes.csv")
+clr_tag_codes <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\tag_codes.csv")%>%
+  mutate_all(as.character)
+clr_upos_codes <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\upos_codes.csv")%>%
+  mutate_all(as.character)
 
 
 clr_stops <- scan("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\corenlp_stops.txt",what="char",sep="\n")
 
-clr_search_syntax <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\search_syntax.csv")
+clr_search_syntax <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\search_syntax.csv")%>%
+  mutate_all(as.character)
 
 setwd("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr")
 #Output
