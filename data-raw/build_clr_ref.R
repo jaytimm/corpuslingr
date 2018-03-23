db51edb902ad339dd747eca80c1a78479327d86c
Jason Timm <jaetimm@gmail.com>
2018-03-22 20:04:57 -0600

    re

--- data-raw/build_clr_ref.R
+++ data-raw/build_clr_ref.R
@@ -3,21 +3,21 @@
 library(tidyverse)
 #devtools::use_data_raw()
 
-clr_ref_penntags <- read.csv("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_penntags.csv")%>%
+clr_ref_penntags <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_penntags.csv")%>%
   mutate_all(as.character)
-clr_ref_upos <- read.csv("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_upos.csv")%>%
-  mutate_all(as.character)
-
-
-clr_ref_stops <- scan("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_stops.txt",what="char",sep="\n")
-
-clr_ref_pos_syntax <- read.csv("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_pos_syntax.csv")%>%
+clr_ref_upos <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_upos.csv")%>%
   mutate_all(as.character)
 
-clr_ref_search_egs <- read.csv("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_search_egs.csv")%>%
+
+clr_ref_stops <- scan("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_stops.txt",what="char",sep="\n")
+
+clr_ref_pos_syntax <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_pos_syntax.csv")%>%
   mutate_all(as.character)
 
-setwd("C:\\Users\\jtimm\\Google Drive\\GitHub\\packages\\corpuslingr")
+clr_ref_search_egs <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_search_egs.csv")%>%
+  mutate_all(as.character)
+
+setwd("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr")
 #Output
 devtools::use_data(clr_ref_penntags, overwrite=TRUE)
 devtools::use_data(clr_ref_upos, overwrite=TRUE)
