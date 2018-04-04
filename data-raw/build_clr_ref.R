b37bda96a07482fa29da909bf56e2652135665fa
Jason Timm <jaetimm@gmail.com>
2018-04-04 10:44:37 -0600

    re

--- data-raw/build_clr_ref.R
+++ data-raw/build_clr_ref.R
@@ -5,13 +5,13 @@ library(tidyverse)
 
 clr_ref_penntags <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_penntags.csv")%>%
   mutate_all(as.character)
-clr_ref_upos <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_upos.csv")%>%
+clr_ref_upos_codes <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_upos_codes.csv")%>%
   mutate_all(as.character)
 
 
 clr_ref_stops <- scan("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_stops.txt",what="char",sep="\n")
 
-clr_ref_pos_syntax <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_pos_syntax.csv")%>%
+clr_ref_pos_codes <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_pos_codes.csv")%>%
   mutate_all(as.character)
 
 clr_ref_search_egs <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr\\data-raw\\clr_ref_search_egs.csv")%>%
@@ -20,9 +20,9 @@ clr_ref_search_egs <- read.csv("C:\\Users\\jason\\Google Drive\\GitHub\\packages
 setwd("C:\\Users\\jason\\Google Drive\\GitHub\\packages\\corpuslingr")
 #Output
 devtools::use_data(clr_ref_penntags, overwrite=TRUE)
-devtools::use_data(clr_ref_upos, overwrite=TRUE)
+devtools::use_data(clr_ref_upos_codes, overwrite=TRUE)
 devtools::use_data(clr_ref_stops, overwrite=TRUE)
-devtools::use_data(clr_ref_pos_syntax, overwrite=TRUE)
+devtools::use_data(clr_ref_pos_codes, overwrite=TRUE)
 devtools::use_data(clr_ref_search_egs, overwrite=TRUE)
 
 #We need consistent names for all 'resource' tables.
